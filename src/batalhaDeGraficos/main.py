7581bb99e2316fbf2b65afdb3cc2bf53062d112a
MGmanfre <miguelmanfre0603@gmail.com>
2026-03-08 18:50:39 -0300

    Added main.ipynb with data analysis and visualization for the e-commerce dataset.

--- src/batalhaDeGraficos/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-df = pd.read_csv('dataset_ecommerce.csv')
-
-melhoresAvaliacoes = df.sort_values('review_score', ascending=False).head(2)
-melhoresAvaliacoes[ ['product_name', 'review_score'] ]
