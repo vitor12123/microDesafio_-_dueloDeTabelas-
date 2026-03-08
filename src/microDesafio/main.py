aa54ee766297268afb1684ca876234be314087ae
vitor12123 <vitorpallis@gmail.com>
2026-03-08 19:29:36 -0300

    ADD/FIX: adicionei grafico de barras e arrumei o arquivo csv

--- src/microDesafio/main.py
+++ src/microDesafio/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-df = pd.read_csv('/home/tristeza/Documents/trabalho mae/projetoPessoal/dataScience_desafio/microDesafio_-_dueloDeTabelas-/src/microDesafio/planilhaCopa.csv')
+df = pd.read_csv('planilhaCopa.csv')
 
 df["total_gols"] = 0
 for i in range(len(df['id_partida'])):
@@ -22,4 +22,16 @@ plt.plot(x, y, z)
 plt.xlabel('qual partida da copa de 2022')
 plt.ylabel('quantidade de gols do Brasil')
 plt.title('Gols do Brasil por rodada da Copa')
+plt.legend(['Gols do Brasil a cada partida', 'Média de Gols de todas as partidas'])
+plt.show()
+
+gols_por_adversario = df.groupby("time_inimigo")["numero_gols_Brasil"].sum()
+
+plt.figure()
+gols_por_adversario.plot(kind="bar")
+plt.xlabel("Adversários do Brasil")
+plt.ylabel("Gols marcados pelo Brasil")
+plt.title("Gols do Brasil por Adversário")
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.show()
